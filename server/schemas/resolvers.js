ce985cb098da93d499a0bfcd4228ca2d551299c7
Avni <avni0000patel@gmail.com>
2022-10-19 15:32:23 -0400

    Define the necessary Query and Mutation types

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -53,7 +53,7 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in!');
         },
-        deleteBook: async (parent, args, context) => {
+        removeBook: async (parent, args, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
