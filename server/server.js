d5a903d22d824681102714e5af5a44c8d7de1aba
Avni <avni0000patel@gmail.com>
2022-10-19 14:23:09 -0400

    Update the auth middleware function to work with the GraphQL API and implement the Apollo Server and apply it to the Express server as middleware

--- server/server.js
+++ server/server.js
@@ -25,7 +25,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -38,29 +37,3 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
 };
-
-// Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
-
-
-// const express = require('express');
-// const path = require('path');
-// const db = require('./config/connection');
-// const routes = require('./routes');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
