167e94538688e42be9a6de57b149346f24b8b51c
Avni <avni0000patel@gmail.com>
2022-10-19 22:14:38 -0400

    working save book and delete book

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -71,16 +71,14 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      // eslint-disable-next-line
+      const { data } = await saveBook({
+        variables: { input: { ...bookToSave } },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
+    } catch (e) {
+      console.error(e);
     }
   };
 
